5eb6947111dcbc7776c83c42377b950dc664938a
Victor Sanchez <victor.san@hotmail.com>
2024-04-27 16:39:48 -0300

    updated sql script

--- database/populate_database.sql
+++ database/populate_database.sql
@@ -25,10 +25,10 @@ INSERT INTO disciplina (nome, descricao, cod_curso) VALUES
 ('Banco de Dados', 'Conceitos e práticas sobre bancos de dados', 3);
 
 -- Populando tabela atividade_complementar
-INSERT INTO atividade_complementar (titulo, descricao, caminho_anexo, horas_solicitadas, data, horas_aprovadas, RA_aluno) VALUES
-('Curso de Python', 'Curso online de Python para iniciantes', '/anexos/curso_python.pdf', 20, '2023-01-15', 20, 1),
-('Workshop de IA', 'Participação no workshop de Inteligência Artificial', '/anexos/workshop_ia.pdf', 15, '2023-02-20', 15, 2),
-('Seminário de TI', 'Seminário sobre novas tecnologias em TI', '/anexos/seminario_ti.pdf', 10, '2023-03-10', 10, 3);
+INSERT INTO atividade_complementar (titulo, descricao, caminho_anexo, horas_solicitadas, data, RA_aluno) VALUES
+('Curso de Python', 'Curso online de Python para iniciantes', '/anexos/curso_python.pdf', 20, '2023-01-15', 1),
+('Workshop de IA', 'Participação no workshop de Inteligência Artificial', '/anexos/workshop_ia.pdf', 15, '2023-02-20', 2),
+('Seminário de TI', 'Seminário sobre novas tecnologias em TI', '/anexos/seminario_ti.pdf', 10, '2023-03-10', 3);
 
 -- Populando curso_disciplina
 INSERT INTO curso_disciplina (cod_curso, cod_disciplina) VALUES
