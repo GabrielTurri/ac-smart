de4c915616e57cc81d201491326f0efc7bd6aff3
Victor Sanchez <victor.san@hotmail.com>
2024-04-27 15:40:53 -0300

    add database folder and sql instructions

---
+++ database/create_database.sql
@@ -0,0 +1,71 @@
+CREATE DATABASE humanitae_db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+USE humanitae_db;
+
+-- Criação da tabela Coordenador
+CREATE TABLE coordenador (
+    cod_coordenador INT PRIMARY KEY AUTO_INCREMENT,
+    nome_coordenador VARCHAR(50),
+    sobrenome_coordenador VARCHAR(50),
+    email_coordenador VARCHAR(60)
+);
+
+-- Criação da tabela Curso
+CREATE TABLE curso (
+    cod_curso INT PRIMARY KEY AUTO_INCREMENT,
+    nome_curso VARCHAR(50),
+    horas_complementares INT,
+    coordenador_curso INT,
+    FOREIGN KEY (coordenador_curso) REFERENCES coordenador(cod_coordenador)
+);
+
+-- Criação da tabela Aluno
+CREATE TABLE aluno (
+    RA_aluno INT PRIMARY KEY AUTO_INCREMENT,
+    nome_aluno VARCHAR(50),
+    sobrenome_aluno VARCHAR(50),
+    email_aluno VARCHAR(60),
+    cod_curso INT,
+    FOREIGN KEY (cod_curso) REFERENCES curso(cod_curso)
+);
+
+-- Criação da tabela Disciplina
+CREATE TABLE disciplina (
+    cod_disciplina INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(150),
+    descricao TINYTEXT,
+    cod_curso INT,
+    FOREIGN KEY (cod_curso) REFERENCES curso(cod_curso)
+);
+
+-- Criação da tabela Atividade_complementar
+CREATE TABLE atividade_complementar (
+    cod_atividade INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(150),
+    descricao TINYTEXT,
+    caminho_anexo VARCHAR(255),
+    horas_solicitadas SMALLINT UNSIGNED,
+    data DATE,
+    status ENUM('Aprovado', 'Reprovado', 'Pendente') DEFAULT 'Pendente',
+    horas_aprovadas INT,
+    RA_aluno INT,
+    FOREIGN KEY (RA_aluno) REFERENCES aluno(RA_aluno)
+);
+
+-- Tabela de junção para Curso e Disciplina
+CREATE TABLE curso_disciplina (
+    cod_curso INT,
+    cod_disciplina INT,
+    PRIMARY KEY (cod_disciplina, cod_curso),
+    FOREIGN KEY (cod_disciplina) REFERENCES disciplina(cod_disciplina),
+    FOREIGN KEY (cod_curso) REFERENCES curso(cod_curso)
+);
+
+-- Tabela de junção para Curso e Aluno
+CREATE TABLE curso_aluno (
+    cod_curso INT,
+    RA_aluno INT,
+    PRIMARY KEY (cod_curso, RA_aluno),
+    FOREIGN KEY (cod_curso) REFERENCES curso(cod_curso),
+    FOREIGN KEY (RA_aluno) REFERENCES aluno(RA_aluno)
+);
