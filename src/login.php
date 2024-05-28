35edabf92a6020f1a6543fb91288ffdfcaf17bd6
Gabriel Turri <ga-briel.t.a@hotmail.com>
2024-05-27 23:27:11 -0300

    finalizando css login

--- src/login.php
+++ src/login.php
@@ -11,7 +11,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 
   <link rel="stylesheet" href="styles/global.css">
-  <link rel="stylesheet" href="styles/styles-login.css">
+  <link rel="stylesheet" href="styles/login.css">
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
